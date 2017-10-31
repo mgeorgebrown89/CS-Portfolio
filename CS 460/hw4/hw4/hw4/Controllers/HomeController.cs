9cd0969c1597a01e29037cb36bbe6c2f35171969
mgeorgebrown89 <m.george.brown89@gmail.com>
2017-10-30 17:38:24 -0700

    work on page 1 feature

--- CS 460/hw4/hw4/hw4/Controllers/HomeController.cs
+++ CS 460/hw4/hw4/hw4/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,5 +17,35 @@ namespace hw4.Controllers
             return View();
         }
 
+        // GET: Home/Page1
+        [HttpGet]
+        public ActionResult Page1()
+        {
+            ViewBag.RequestMethod = "GET";
+            return View();
+        }
+
+        // POST: Home/Page1
+        [HttpPost]
+        public ActionResult Page1(string name, string password, int? age)
+        {
+            ViewBag.Message = "This is Page1.";
+            ViewBag.RequestMethod = "POST";
+            // What to do if we don't get something we need?  For now let's just
+            // tell the client they're "Bad" :-)
+            if (age == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Looks good, we can use this data
+            Debug.WriteLine($"{name}:{password}:{age}");
+
+            // And then redirect the user to another page (if desired)
+            //return RedirectToAction("Index");
+
+            // or return the same page
+            return View();
+        }
     }
 }
