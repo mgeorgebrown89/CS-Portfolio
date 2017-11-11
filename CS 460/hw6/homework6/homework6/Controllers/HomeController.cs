afaaf7b0e11bc59aa84f7247546a4cc814eed42b
mgeorgebrown89 <m.george.brown89@gmail.com>
2017-11-11 14:16:52 -0800

    got mountainbikes to display

---
+++ CS 460/hw6/homework6/homework6/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using homework6.Models;
+
+namespace homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AdvWorksContext db = new AdvWorksContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        //GET: Home/MountainBikes
+        public ActionResult MountainBikes()
+        {
+            var mBikes = db.Products.Where(s => s.ProductSubcategory.Name == "Mountain Bikes");
+
+            return View(mBikes);
+        }
+    }
+}
