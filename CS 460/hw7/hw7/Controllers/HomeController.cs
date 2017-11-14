ddb8ef3cc940d401fadf515306688c80e1e2242f
mgeorgebrown89 <m.george.brown89@gmail.com>
2017-11-13 20:11:15 -0800

    original save of hw7 in wrong place, initial commit now. got example from class working.

---
+++ CS 460/hw7/hw7/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hw7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Home/RandomNumbers/10
+        public JsonResult RandomNumbers(int? id = 100)
+        {
+            Random gen = new Random();
+            int[] arr = new int[(int)id];       // yes, no checking at the moment
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = gen.Next(1000);
+            }
+
+            var data = new
+            {
+                message = "Hello from SimpleAPI",
+                num = (int)id,
+                numbers = arr
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CS 460/hw7/hw7/Scripts/Search.js
@@ -0,0 +1,31 @@
+﻿//console.log("In numbers.js")
+
+$("#Request").click(function () {
+    var a = $("#Count").val();
+    console.log(a);
+    var source = "/Home/RandomNumbers/" + a;
+    console.log(source);
+
+    // Send an async request to our server, requesting JSON back
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: displayData,
+        error: errorOnAjax
+    });
+});
+
+function displayData(data) {
+    console.log(data);
+    $("#Message").text(data["message"]);
+    $("#Amount").text("Number of values requested " + data.num);
+    $("#Values").text(data.numbers);
+    var sum = data.numbers.reduce(function (a, b) { return a + b; });
+    var ave = sum / data.numbers.length;
+    $("#Average").text("Average of these values is " + ave);
+}
+
+function errorOnAjax() {
+    console.log("error");
+}
