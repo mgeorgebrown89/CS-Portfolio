cb306b57d1147f86fc311211c664d8695b2c4e63
mgeorgebrown89 <m.george.brown89@gmail.com>
2017-11-30 21:06:57 -0800

    implemented everything. LINQ sucks

--- CS-460/hw8/hw8/hw8/Controllers/HomeController.cs
+++ CS-460/hw8/hw8/hw8/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using hw8.Models;
 
 namespace hw8.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtistContext db = new ArtistContext();
+
         public ActionResult Index()
         {
-            return View();
+            var genres = db.Genres.ToList();
+            return View(genres);
+        }
+
+        public JsonResult Genre(int id)
+        {
+            var works = db.Genres.Find(id)
+                                 .Classifications
+                                 .ToList()
+                                 .OrderBy(t => t.Artwork.Title)
+                                 .Select(a => new { aw = a.ArtworkID, awa = a.Artwork.ArtistID })
+                                 .ToList();
+
+            string[] artworkCreator = new string[works.Count()];
+            for (int i = 0; i < artworkCreator.Length; ++i)
+            {
+                artworkCreator[i] = $"<ul>{db.Artworks.Find(works[i].aw).Title} by {db.Artists.Find(works[i].awa).Name}</ul>";
+            }
+            var data = new
+            {
+                arr = artworkCreator
+            };
+            
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
